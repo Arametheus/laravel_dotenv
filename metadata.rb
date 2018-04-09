4b43c7aaa16ed217764b4be75c5414288fbd4bf9
Troy Montour <vampiresinc@gmail.com>
2018-04-09 12:53:53 -0500

    Add files via upload
    This is to create laravel php env file off of AWS settings.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              "laravel_dotenv"
+maintainer        "Arametheus"
+maintainer_email  "tmontour@gmail.com"
+license           "MIT"
+description       "Creates .env file from Stack json"
+version           "1.0.0"
+
