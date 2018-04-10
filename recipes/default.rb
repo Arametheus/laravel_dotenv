a7b8ab068945aa4df28184f37f5b43b8aaaa518e
Troy Montour <vampiresinc@gmail.com>
2018-04-10 12:46:09 -0500

    Update default.rb
    changing file for AWS chef 12

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,26 @@
-node[:deploy].each do |application, deploy|
+#node[:deploy].each do |application, deploy|
   # write out .env file
-  template "#{deploy[:deploy_to]}/current/.env" do
+#  template "#{deploy[:deploy_to]}/current/.env" do
+#    source 'env.erb'
+#    mode '0660'
+#    user deploy[:user]
+#    group deploy[:group]
+#    variables(
+#      :env => deploy[:environment_variables]
+#    )
+#  end
+#end
+
+
+search("aws_opsworks_app").each do |app|
+    # write out .env file
+  template "/srv/www/#{app[:shortname]}/current/.env" do
     source 'env.erb'
     mode '0660'
     user deploy[:user]
     group deploy[:group]
     variables(
-      :env => deploy[:environment_variables]
+      :env => app[:environment]
     )
   end
 end
